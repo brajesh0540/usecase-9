313b62272824953cf32ff5af307f457b9aea785d
brajesh0540 <36298862+brajesh0540@users.noreply.github.com>
2025-08-12 14:33:23 +0530

    Update eks.tf

--- eks.tf
+++ eks.tf
@@ -1,49 +1,44 @@
-resource "aws_iam_role" "demo-eks-cluster-role" {
-name = "demo-eks-cluster-role"
-assume_role_policy = jsonencode({
+resource "aws_eks_cluster" "eks-cluster" {
+  name = var.cluster_name
+
+  role_arn = aws_iam_role.eks-cluster.arn
+
+  vpc_config {
+    subnet_ids = var.aws_public_subnet
+    endpoint_public_access  = true
+    public_access_cidrs = var.public_access_cidrs
+    security_group_ids = [aws_security_group.node_group_one.id]
+  }
+
+  # Ensure that IAM Role permissions are created before and deleted
+  # after EKS Cluster handling. Otherwise, EKS will not be able to
+  # properly delete EKS managed EC2 infrastructure such as Security Groups.
+  depends_on = [
+    aws_iam_role_policy_attachment.cluster_AmazonEKSClusterPolicy,
+    
+  ]
+}
+
+resource "aws_iam_role" "eks-cluster-role" {
+  name = "eks-cluster-example"
+  assume_role_policy = jsonencode({
     Version = "2012-10-17"
     Statement = [
-    {
+      {
         Action = [
-        "sts:AssumeRole",
+          "sts:AssumeRole",
+          "sts:TagSession"
         ]
         Effect = "Allow"
         Principal = {
-        Service = "eks.amazonaws.com"
-
+          Service = "eks.amazonaws.com"
         }
-    },
+      },
     ]
-})
+  })
 }
 
-resource "aws_iam_role_policy_attachment" "eks_cluster_policy" {
-policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
-role       = aws_iam_role.demo-eks-cluster-role.name
-}
-
-resource "aws_eks_cluster" "demo-eks-cluster" {
-    name = var.cluster_name
-    role_arn = aws_iam_role.demo-eks-cluster-role.arn
-    vpc_config {
-    endpoint_private_access = true
-    endpoint_public_access = true
-    subnet_ids = [
-        aws_subnet.private-subnet-1.id,
-        aws_subnet.private-subnet-2.id,
-        aws_subnet.public-subnet-1.id,
-        aws_subnet.public-subnet-2.id
-    ]
-    }
-    access_config {
-    authentication_mode = "API"
-    bootstrap_cluster_creator_admin_permissions = true
-    }
-    bootstrap_self_managed_addons = true
-    tags = var.tags
-    version = var.eks_version
-    upgrade_policy {
-    support_type = "STANDARD"
-    }
-    depends_on = [ aws_iam_role_policy_attachment.eks_cluster_policy ]
+resource "aws_iam_role_policy_attachment" "cluster_AmazonEKSClusterPolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+  role       = aws_iam_role.cluster.name
 }
