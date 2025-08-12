cde88a0ae5e6ebdc9f650079b4b6061f3c3d1a2e
brajesh0540 <36298862+brajesh0540@users.noreply.github.com>
2025-08-12 15:08:53 +0530

    Update eks.tf

--- eks.tf
+++ eks.tf
@@ -40,5 +40,5 @@ resource "aws_iam_role" "eks-cluster-role" {
 
 resource "aws_iam_role_policy_attachment" "cluster_AmazonEKSClusterPolicy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
-  role       = aws_iam_role.cluster.name
+  role       = aws_iam_role.eks-cluster-role.name
 }
