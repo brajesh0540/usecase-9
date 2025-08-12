399d204f8cb9ae58130dcc40c0eb01a23c7a8b7e
brajesh0540 <36298862+brajesh0540@users.noreply.github.com>
2025-08-12 23:01:40 +0530

    Update fargate.tf

--- fargate.tf
+++ fargate.tf
@@ -2,7 +2,6 @@ resource "aws_eks_fargate_profile" "eks-fg-pf" {
   cluster_name           = aws_eks_cluster.eks-cluster.name
   fargate_profile_name   = "eks-profile1"
   pod_execution_role_arn = aws_iam_role.example.arn
-  subnet_ids             = aws_subnet.example[*].id
   selector {
     namespace = "kube-system"
   }
